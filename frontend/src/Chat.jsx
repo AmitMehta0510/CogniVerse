a37e3dd1b5ce4430d5fe829154afdf8b2a8510bb
Amit Mehta <amitmehta0510@gmail.com>
2025-10-15 03:11:06 +0530

    Formats gpt response code blocks

--- frontend/src/Chat.jsx
+++ frontend/src/Chat.jsx
@@ -1,6 +1,10 @@
 import "./Chat.css";
 import { useContext } from "react";
 import MyContext from "./MyContext";
+import ReactMarkdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css";
+//react-markdown, rehype-highlight for formating code blocks
 
 function Chat() {
   const { newChat, prevChats } = useContext(MyContext);
@@ -9,13 +13,18 @@ function Chat() {
     <>
       {newChat && <h1>Start a New Chat</h1>}
       <div className="chats">
-        {prevChats.map((chat, index) => {
-          <div className={chat.role === "user" ? "userDiv" : "gptDiv"}>
+        {prevChats.map((chat, idx) => {
+          <div
+            className={chat.role === "user" ? "userDiv" : "gptDiv"}
+            key={idx}
+          >
             {chat.role === "user" ? (
               <p className="userMessage">{chat.content}</p>
             ) : (
-              <p className="gptMessage">{chat.content}</p>
-            )} 
+              <ReactMarkdown rehypePlugins={[rehypeHighlight]}>
+                {chat.content}
+              </ReactMarkdown>
+            )}
           </div>;
         })}
       </div>
