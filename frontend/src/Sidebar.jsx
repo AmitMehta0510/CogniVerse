325661f46c599131694476ef9fe1a74626e8e335
Amit Mehta <amitmehta0510@gmail.com>
2025-10-15 00:25:53 +0530

    sidebar UI done

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -2,9 +2,28 @@ import "./Sidebar.css";
 
 function Sidebar() {
   return (
-    <div className="sidebar">
-      <h2>Sidebar</h2>
-    </div>
+    <section className="sidebar">
+      {/* new chat button */}
+      <button>
+        <img className="logo" src="src/assets/blacklogo.png" alt="logo" />
+        <span>
+          <i className="fa-solid fa-pen-to-square"></i>
+        </span>
+      </button>
+
+      {/* history */}
+      <div className="history">
+        <ul>
+          <li>Thread1: Hello</li>
+          <li>Thread2: Hi there!</li>
+        </ul>
+      </div>
+
+      {/* sign */}
+      <div className="sign">
+        <p>Made with ❤️ by Amit Mehta</p>
+      </div>
+    </section>
   );
 }
 export default Sidebar;
